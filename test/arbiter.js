7b13a72802aaff13830042427a20198694664907
Nicholas Wold <nickw254@gmail.com>
2016-03-05 12:48:24 -0800

    Some tests exist!

---
+++ test/arbiter.js
@@ -0,0 +1,39 @@
+var should = require('chai').should(),
+    arbiter = require('../index'),
+    loadmap = arbiter.loadmap;
+
+describe('#loadmap', function() {
+    //var map = loadmap(); // The traditional diplomacy map gets loaded by default
+
+    it('loads the traditional diplomacy map', function() {
+        false.should.equal(true);
+    });
+
+    it('has the proper movement graph', function () {
+        false.should.equal(true);
+    });
+
+    it('has identifying abbreviations', function () {
+        false.should.equal(true);
+    });
+
+    it('specifies number of players and their countries', function () {
+        false.should.equal(true);
+    });
+
+    it('specifies supply depots', function () {
+        false.should.equal(true);
+    });
+
+    it('has home countries', function () {
+        false.should.equal(true);
+    });
+
+    it('specifies types of territories', function () {
+        false.should.equal(true);
+    });
+
+    it('displays the full name if possible', function () {
+        false.should.equal(true);
+    });
+});
