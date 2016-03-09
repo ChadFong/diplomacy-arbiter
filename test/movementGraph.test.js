e71fb6dd6cb79091ca0900f52ec6c4328ba10854
ChadFong <ChadMFong@Gmail.com>
2016-03-08 23:40:20 -0800

    update according to pull request comments
    - Abstract movementGraphs to support multiple maps
      - pluralize file and nest under 'diplomacy'.
    
    - removed abbrData because that should only have lived on a personal
      branch and never seen the light of day whooooops.
    
    - updated test

--- test/movementGraph.test.js
+++ test/movementGraph.test.js
@@ -1,20 +1,23 @@
 var should = require('chai').should(),
-    movementGraph = require('../mapData/movementGraph.js');
+    diploGraph = require('../mapData/movementGraphs.js').diplomacy;
 
 describe('movement graph connection data', function() {
-    it('all connections should be valid', function () {
-        for(var territory in movementGraph) {
-            movementGraph[territory].landConns.forEach(function (terr) {
-                var conn = movementGraph[terr];
-                if(conn === undefined) {console.log(terr);}
-                movementGraph[terr].should.not.equal(undefined);
-            });
+  it('all connections should be valid', function () {
+    var currTerr;
+    for(var terr in diploGraph) {
+      currTerr = diploGraph[terr];
 
-            movementGraph[territory].seaConns.forEach(function (terr) {
-                var conn = movementGraph[terr];
-                if(conn === undefined) {console.log(terr);}
-                movementGraph[terr].should.not.equal(undefined);
-            });
-        }
-    });
+      if(diploGraph[terr].landConns !== undefined) {
+        diploGraph[terr].landConns.forEach(function (conn) {
+          diploGraph[conn].should.not.equal(undefined);
+        });                
+      }
+      
+      if(currTerr.seaConns !== undefined) {
+        diploGraph[terr].seaConns.forEach(function (conn) {
+          diploGraph[conn].should.not.equal(undefined);
+        });
+      }
+    }
+  });
 });
